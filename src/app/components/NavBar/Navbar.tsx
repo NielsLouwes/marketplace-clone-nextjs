b47ffb81bfe428f3bf35d874de8348e679d68c61
Niels Louwes <niels.louwes@and.digital>
2023-09-18 16:19:10 +0200

    added favorites page

--- src/app/components/NavBar/Navbar.tsx
+++ src/app/components/NavBar/Navbar.tsx
@@ -1,13 +1,25 @@
 import styled from "styled-components";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import StarBorderOutlinedIcon from "@mui/icons-material/StarBorderOutlined";
+import Link from "next/link";
 
 const Styled = styled.div`
-  background-color: #FF323C;
-  padding: 5px;
-`
+  background-color: #ff323c;
+  /* padding: 5px; */
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 export default function NavBar() {
   return (
     <Styled>
       <h1>Marketplace</h1>
+      <div>
+        <Link href="/favorites">
+          <StarBorderOutlinedIcon fontSize="large" />
+        </Link>
+        <ShoppingCartOutlinedIcon fontSize="large" />
+      </div>
     </Styled>
-  )
+  );
 }
---
+++ src/pages/favorites.tsx
@@ -0,0 +1,8 @@
+/* to do
+1. Sold or not sold status on favorite items
+2. show item details, current bid etc.
+*/
+
+export default function Favorites() {
+  return (<><h2>Favorite Items</h2></>);
+}
